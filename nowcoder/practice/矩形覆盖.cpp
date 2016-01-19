ed0244e7f8ce0b88f6ead9535881dce34e6a4825
siukwan <siukwan@users.noreply.github.com>
2016-01-19 03:14:06 -0800

    增加：矩阵覆盖.cpp

---
+++ nowcoder/practice/矩形覆盖.cpp
@@ -0,0 +1,32 @@
+﻿/*
+矩形覆盖
+
+题目描述
+我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//类似斐波那契数列
+class Solution {
+public:
+	int rectCover(int number) {
+		if (number == 0) return 1;
+		vector<int> dp(number + 2, 0);
+		dp[0] = 1;
+		dp[1] = 2;
+		for (int i = 2; i < number; ++i)
+			dp[i] = dp[i - 1] + dp[i - 2];
+		return dp[number - 1];
+
+	}
+};
+int main()
+{
+	Solution sols;
+	cout << sols.rectCover(5) << endl;
+	return 0;
+}
