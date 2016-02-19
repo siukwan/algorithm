e25293f735803365656723ed568b1ffaef860a8d
siukwan <siukwan@users.noreply.github.com>
2016-02-19 16:11:21 +0800

    数值的整数次方

---
+++ nowcoder/practice/数值的整数次方.cpp
@@ -0,0 +1,22 @@
+﻿/*
+题目描述
+
+给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
+
+主要需要考虑幂为0和为负数的情况
+*/
+class Solution {
+public:
+	double Power(double base, int exponent) {
+		double result = 1;
+		if (exponent == 0) return 1;
+		else if (exponent<0)
+		{
+			base = 1 / base;
+			exponent = -exponent;
+		}
+		for (int i = 0; i<exponent; i++)
+			result *= base;
+		return result;
+	}
+};
