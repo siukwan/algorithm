43c0637e7a3d4087e9b640d719d30d807231b8cf
siukwan <siukwan@users.noreply.github.com>
2016-02-21 10:32:55 +0800

    两个链表的第一个公共结点

---
+++ nowcoder/practice/两个链表的第一个公共结点.cpp
@@ -0,0 +1,34 @@
+﻿/*
+题目描述
+
+输入两个链表，找出它们的第一个公共结点。
+*/
+/*
+struct ListNode {
+int val;
+struct ListNode *next;
+ListNode(int x) :
+val(x), next(NULL) {
+}
+};*/
+class Solution {
+public:
+	ListNode* FindFirstCommonNode(ListNode *pHead1, ListNode *pHead2) {
+		map<ListNode *, int>m;
+		ListNode *tmp;
+		tmp = pHead1;
+		while (tmp)
+		{
+			m[tmp]++;
+			tmp = tmp->next;
+		}
+		tmp = pHead2;
+		while (tmp)
+		{
+			m[tmp]++;
+			if (m[tmp] == 2) break;
+			tmp = tmp->next;
+		}
+		return tmp;
+	}
+};
