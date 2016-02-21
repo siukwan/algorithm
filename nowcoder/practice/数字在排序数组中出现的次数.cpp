44c175f340ff8197cc3570193fab0699def557e8
siukwan <siukwan@users.noreply.github.com>
2016-02-21 14:14:30 +0800

    数字在排序数组中出现的次数

---
+++ nowcoder/practice/数字在排序数组中出现的次数.cpp
@@ -0,0 +1,47 @@
+﻿/*
+题目描述
+
+统计一个数字在排序数组中出现的次数。
+*/
+class Solution {
+public:
+	int GetNumberOfK(vector<int> data, int k) {
+		if (data.size() == 0) return 0;
+		int l = 0;
+		int r = data.size() - 1;
+		int mid = (l + r) / 2;
+
+		while (l<r)
+		{
+			mid = (l + r) / 2;
+			if (data[mid] == k)
+				break;
+			else if (data[mid]<k)
+				l = mid + 1;
+			else
+				r = mid - 1;
+		}
+
+		if (data[mid] == k)
+		{
+			int count = 0;
+			for (int i = mid; i >= 0; --i)
+			{
+				if (data[i] == k)
+					count++;
+				else break;
+			}
+			for (int i = mid + 1; i<data.size(); ++i)
+			{
+				if (data[i] == k)
+					count++;
+				else break;
+			}
+			return count;
+
+		}
+		else
+			return 0;
+	}
+
+};
