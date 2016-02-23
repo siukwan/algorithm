a7f1f92927a940a3e3c855c6dc112b89564bfe1b
siukwan <siukwan@users.noreply.github.com>
2016-02-23 20:44:59 +0800

    数组中重复的数字

---
+++ nowcoder/practice/数组中重复的数字.cpp
@@ -0,0 +1,39 @@
+﻿/*
+题目描述
+
+在一个长度为n的数组里的所有数字都在0到n-1的范围内。 数组中某些数字是重复的，但不知道有几个数字是重复的。也不知道每个数字重复几次。请找出数组中任意一个重复的数字。 例如，如果输入长度为7的数组{2,3,1,0,2,5,3}，那么对应的输出是重复的数字2或者3。
+
+*/
+class Solution {
+public:
+	// Parameters:
+	//        numbers:     an array of integers
+	//        length:      the length of array numbers
+	//        duplication: (Output) the duplicated number in the array number
+	// Return value:       true if the input is valid, and there are some duplications in the array number
+	//                     otherwise false
+	bool duplicate(int numbers[], int length, int* duplication) {
+		map<int, int> m;
+		vector<int> result(0);
+		for (int i = 0; i<length; ++i)
+		{
+			m[numbers[i]]++;
+		}
+
+		map<int, int>::iterator ite = m.begin();
+
+		for (; ite != m.end(); ite++)
+		{
+			if (ite->second>1)
+			{
+				result.push_back(ite->first);
+				break;
+			}
+		}
+
+		if (result.size() == 0) return false;
+
+		*duplication = result[0];
+		return true;
+	}
+};
