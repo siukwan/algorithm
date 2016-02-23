5bdb8213fff9f6eb9f1764436f072e7b8ee67536
siukwan <siukwan@users.noreply.github.com>
2016-02-23 20:20:41 +0800

    不用加减乘除做加法

---
+++ nowcoder/practice/不用加减乘除做加法.cpp
@@ -0,0 +1,23 @@
+﻿/*
+题目描述
+
+写一个函数，求两个整数之和，要求在函数体内不得使用+、-、*、/四则运算符号。
+*/
+class Solution {
+public:
+	int Add(int num1, int num2)
+	{
+		int carry;
+		int sum;
+		do
+		{
+			sum = num1^num2;
+			carry = (num1&num2) << 1;
+
+			num1 = sum;
+			num2 = carry;
+		} while (carry != 0);
+
+		return sum;
+	}
+};
