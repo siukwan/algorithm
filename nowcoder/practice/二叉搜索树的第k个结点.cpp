fff767a157b248d8a2bdc1aba6954912553d96d4
siukwan <siukwan@users.noreply.github.com>
2016-02-23 19:17:29 +0800

    二叉搜索树的第k个结点

---
+++ nowcoder/practice/二叉搜索树的第k个结点.cpp
@@ -0,0 +1,34 @@
+﻿/*
+题目描述
+
+给定一颗二叉搜索树，请找出其中的第k大的结点。例如， 5 / \ 3 7 /\ /\ 2 4 6 8 中，按结点数值大小顺序第三个结点的值为4。
+
+*/
+/*
+struct TreeNode {
+int val;
+struct TreeNode *left;
+struct TreeNode *right;
+TreeNode(int x) :
+val(x), left(NULL), right(NULL) {
+}
+};
+*/
+class Solution {
+public:
+	TreeNode* KthNode(TreeNode* pRoot, unsigned int k)
+	{
+		vector<TreeNode*> in(0);
+		dfs(pRoot, in);
+		if (in.size()<k || k == 0 || in.size() == 0) return NULL;
+		else return in[k - 1];
+	}
+	void dfs(TreeNode* root, vector<TreeNode*>&in)
+	{
+		if (root == NULL) return;
+		dfs(root->left, in);
+		in.push_back(root);
+		dfs(root->right, in);
+	}
+
+};
