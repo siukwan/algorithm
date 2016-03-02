4a6695c1dc1ec1f064c3394dccf0b2f4ef0ab0ac
siukwan <siukwan@users.noreply.github.com>
2016-03-02 08:43:55 +0800

    整数转化

---
+++ nowcoder/Cracking the Coding Interview/整数转化.cpp
@@ -0,0 +1,25 @@
+﻿/*
+编写一个函数，确定需要改变几个位，才能将整数A转变成整数B。
+给定两个整数int A，int B。请返回需要改变的数位个数。
+测试样例：
+10,5
+返回：4
+
+*/
+class Transform {
+public:
+	int calcCost(int A, int B) {
+		// write code here
+		int y = 1;
+		int result = 0;
+		for (int i = 0; i<32; ++i)
+		{
+			int a = (y&A);
+			int b = (y&B);
+			if (a != b)
+				result++;
+			y = y << 1;
+		}
+		return result;
+	}
+};
