c86ceb2590c52da7e4a5f0a13507b0ffe6422c2d
siukwan <siukwan@users.noreply.github.com>
2016-03-04 22:58:57 +0800

    69. Sqrt(x)

---
+++ leetcode/69. Sqrt(x).cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+	int mySqrt(int x) {
+		int c = 1;
+		int ans = 0;
+		while (1)
+		{
+			int tmp = ans + c;
+			unsigned long long tmp2 = tmp*tmp;
+			if (tmp2 <= x)
+			{
+				ans += c;
+				c = (c << 1);
+			}
+			else
+			{
+				if (c == 1) return ans;
+				c = 1;
+			}
+		}
+	}
+};
