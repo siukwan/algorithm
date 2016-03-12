af5b6227009f2fca3401197d7bf6215dd615e56f
siukwan <siukwan@users.noreply.github.com>
2016-03-12 09:11:33 +0800

    92. Reverse Linked List II

---
+++ leetcode/92. Reverse Linked List II(easy).cpp
@@ -0,0 +1,27 @@
+﻿/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+class Solution {
+public:
+	ListNode *reverseBetween(ListNode *head, int m, int n) {
+		n -= m;
+		ListNode prehead(0);
+		prehead.next = head;
+		ListNode* pre = &prehead;
+		while (--m)pre = pre->next;
+		ListNode* pstart = pre->next;
+		while (n--)
+		{
+			ListNode *p = pstart->next;
+			pstart->next = p->next;
+			p->next = pre->next;
+			pre->next = p;
+		}
+		return prehead.next;
+	}
+};
