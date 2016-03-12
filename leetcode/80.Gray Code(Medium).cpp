aec4c8111eb1d380b53d546ec3238fa8ca864451
siukwan <siukwan@users.noreply.github.com>
2016-03-13 00:08:30 +0800

    gray code

---
+++ leetcode/80.Gray Code(Medium).cpp
@@ -0,0 +1,20 @@
+﻿class Solution {
+public:
+	vector<int> grayCode(int n) {
+		vector<int> gray;
+
+		gray.push_back(0);
+		if (n == 0) return gray;
+		gray.push_back(1);
+
+		for (int digit = 1; digit<n; digit++)
+		{
+			int size = gray.size();
+			for (int i = size - 1; i >= 0; --i)
+			{
+				gray.push_back((1 << digit) + gray[i]);
+			}
+		}
+		return gray;
+	}
+};
