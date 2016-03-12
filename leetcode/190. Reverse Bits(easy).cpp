f1120fad2e5da9fb15bf3bdc3a2663fd163f50b1
siukwan <siukwan@users.noreply.github.com>
2016-03-12 09:10:40 +0800

    190. Reverse Bits

---
+++ leetcode/190. Reverse Bits(easy).cpp
@@ -0,0 +1,12 @@
+﻿class Solution {
+public:
+	uint32_t reverseBits(uint32_t n) {
+
+		n = (n & 0x55555555) << 1 | (n & 0xaaaaaaaa) >> 1;
+		n = (n & 0x33333333) << 2 | (n & 0xcccccccc) >> 2;
+		n = (n & 0x0f0f0f0f) << 4 | (n & 0xf0f0f0f0) >> 4;
+		n = (n & 0x00ff00ff) << 8 | (n & 0xff00ff00) >> 8;
+		n = (n & 0x0000ffff) << 16 | (n & 0xffff0000) >> 16;
+		return n;
+	}
+};
