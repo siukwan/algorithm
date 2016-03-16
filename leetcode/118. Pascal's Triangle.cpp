38430c558d2f6772f7650ebef60b7f9fe46786d2
siukwan <siukwan@users.noreply.github.com>
2016-03-16 08:59:29 +0800

    118. Pascal's Triangle

---
+++ leetcode/118. Pascal's Triangle.cpp
@@ -0,0 +1,22 @@
+﻿class Solution {
+public:
+	vector<vector<int>> generate(int numRows) {
+		vector<vector<int>> result;
+
+		for (int i = 0; i<numRows; ++i)
+		{
+			vector<int> row(i + 1, 1);
+			for (int j = 0; j <= i / 2 && i != 0; ++j)
+			{
+				if (j != 0)
+				{
+					row[j] = result.back()[j - 1] + result.back()[j];
+					row[row.size() - 1 - j] = row[j];
+				}
+
+			}
+			result.push_back(row);
+		}
+		return result;
+	}
+};
