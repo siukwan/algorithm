e7f422f281bcc15620b925cb409895dd114560b0
siukwan <siukwan@users.noreply.github.com>
2016-03-16 09:19:02 +0800

    119. Pascal's Triangle II

---
+++ leetcode/119. Pascal's Triangle II.cpp
@@ -0,0 +1,24 @@
+﻿class Solution {
+public:
+	vector<int> getRow(int rowIndex) {
+		vector<int>result(rowIndex + 1, 1);
+
+		for (int i = 1; i <= (rowIndex + 1) / 2; ++i)
+		{
+			unsigned long long tmp = result[i - 1];
+			if (tmp%i == 0)
+			{
+				tmp /= i;
+				tmp *= (rowIndex + 1 - i);
+			}
+			else
+			{
+				tmp *= (rowIndex + 1 - i);
+				tmp /= i;
+			}
+			result[i] = tmp;
+			result[rowIndex - i] = result[i];
+		}
+		return result;
+	}
+};
