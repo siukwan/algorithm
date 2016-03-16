7a2a3f4cd80451ea38d03457c9ed93e1f3fa3d45
siukwan <siukwan@users.noreply.github.com>
2016-03-16 19:27:39 +0800

    121. Best Time to Buy and Sell Stock

---
+++ leetcode/121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+	int maxProfit(vector<int>& prices) {
+		if (prices.size() <= 1) return 0;
+
+		int low = prices[0], high = 0;
+		int profit = 0;
+		for (int i = 1; i<prices.size(); ++i)
+		{
+			if (prices[i]<low)
+				low = prices[i];
+			else if (prices[i] - low>profit)
+				profit = prices[i] - low;
+		}
+		return profit;
+	}
+};
