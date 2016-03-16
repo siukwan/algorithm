335e1d0826fff05aa8e6a70197039cee815923b7
siukwan <siukwan@users.noreply.github.com>
2016-03-16 18:43:30 +0800

    169. Majority Element

---
+++ leetcode/169. Majority Element.cpp
@@ -0,0 +1,23 @@
+﻿class Solution {
+public:
+	int majorityElement(vector<int>& nums) {
+		if (nums.size() == 0) return 0;
+		int res = nums[0];
+		int count = 1;
+		for (int i = 1; i<nums.size(); ++i)
+		{
+			if (res != nums[i])
+			{
+				count--;
+				if (count == 0)
+				{
+					count = 1;
+					res = nums[++i];
+				}
+			}
+			else
+				count++;
+		}
+		return res;
+	}
+};
