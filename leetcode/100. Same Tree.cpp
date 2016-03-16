20810cb7872d8e25197eab95444981f5079ddf2e
siukwan <siukwan@users.noreply.github.com>
2016-03-16 17:25:29 +0800

    100. Same Tree

---
+++ leetcode/100. Same Tree.cpp
@@ -0,0 +1,26 @@
+﻿/**
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
+class Solution {
+public:
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		if (p == NULL && q == NULL) return true;
+		else if (p == NULL && q != NULL) return false;
+		else if (p != NULL && q == NULL) return false;
+		else
+		{
+			if (p->val == q->val)
+			{
+				return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+			}
+			else
+				return false;
+		}
+	}
+};
