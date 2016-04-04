0e42310ffa56d196e9743beabc51d88484ae6db8
siukwan <siukwan@users.noreply.github.com>
2016-04-04 17:29:02 +0800

    添加json文件

--- python/json/json_test.py
+++ python/json/json_test.py
@@ -1,15 +1,4 @@
-import json
- 
-json_input = '{ "one": 1, "two": { "list": [ {"item":"A"},{"item":"B"} ] } }'
- 
-try:
-    decoded = json.loads(json_input)
- 
-    # pretty printing of json-formatted string
-    print json.dumps(decoded, sort_keys=True, indent=4)
- 
-    print "JSON parsing example: ", decoded['one']
-    print "Complex JSON parsing example: ", decoded['two']['list'][1]['item']
- 
-except (ValueError, KeyError, TypeError):
-    print "JSON format error"
+
+class json:
+	def decode(self, String str):
+		
