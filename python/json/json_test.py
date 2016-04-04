79e47cd7cfcca839922bc0261aa6b36e3292849a
siukwan <siukwan@users.noreply.github.com>
2016-04-04 22:14:53 +0800

    读取输出

--- python/json/json_test.py
+++ python/json/json_test.py
@@ -1,4 +1,25 @@
-
+#encoding: utf-8
+import json
 class json:
-	def decode(self, String str):
-		
+	def decode(self, json_input):
+		print "test"
+
+	def decode_jsonLib(self,json_input):
+		try:
+			decoded = json.loads(json_input)
+			# pretty printing of json-formatted string
+			print json.dumps(decoded, sort_keys=True, indent=4)
+		except (ValueError, KeyError, TypeError):
+			print "JSON format error"
+
+
+
+
+
+
+fp=open('json.txt')
+allLines = fp.readlines()
+#print fp
+fp.close()
+for eachLine in allLines:
+	print eachLine #支持输出换行符
