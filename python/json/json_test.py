af79795f572175294c0073106bc7380975465f1c
siukwan <siukwan@users.noreply.github.com>
2016-04-04 22:51:23 +0800

    暂时无法直接使用json库解析

--- python/json/json_test.py
+++ python/json/json_test.py
@@ -2,8 +2,11 @@
 import sys  
 reload(sys)   
 sys.setdefaultencoding('utf8')  
+
 import json
-class json:
+
+
+class jsonClass:
 	def decode(self, json_input):
 		print "test"
 
@@ -20,18 +23,22 @@ class json:
 def ConvertCN(s):  
 	return s.encode('gb18030')  
 
+#main函数
+if __name__ == '__main__':
 
-fp=open('json.txt')
-allLines = fp.readlines()
-#print fp
+	fp=open('json.txt')
+	allLines = fp.readlines()
+	#print fp
 
 
-fp.close()
-str=""
-for eachLine in allLines:
-	eachLine=ConvertCN(eachLine)
-	for i in range(0,len(eachLine)):
-		if eachLine[i]!= ' ' and eachLine[i]!='\n': #删除空格和换行符，但是json双引号中的内容空格不能删除
-			str+=eachLine[i]
-	#print eachLine #支持输出换行符
-print str
+	fp.close()
+	str=""
+	for eachLine in allLines:
+		eachLine=ConvertCN(eachLine)
+		for i in range(0,len(eachLine)):
+			if eachLine[i]!= ' ' and eachLine[i]!= '	' and eachLine[i]!='\n': #删除空格和换行符，但是json双引号中的内容空格不能删除
+				str+=eachLine[i]
+		#print eachLine #支持输出换行符
+	json_Instance=jsonClass()
+	json_Instance.decode_jsonLib(str)
+	print str
