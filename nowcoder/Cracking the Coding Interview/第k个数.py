361d7460e97888db54286f6d16619ca6c98e3559
siukwan <siukwan@users.noreply.github.com>
2016-04-14 15:31:15 +0800

    第k个数

---
+++ nowcoder/Cracking the Coding Interview/第k个数.py
@@ -0,0 +1,20 @@
+﻿# -*- coding:utf-8 -*-
+class KthNumber:
+    def findKth(self, k):
+        # write code here
+        result=[1]
+        idx=[0,0,0]
+        for i in range(0,k,1):
+            a=result[idx[0]]*3
+            b=result[idx[1]]*5
+            c=result[idx[2]]*7
+            
+            tmp=min(min(a,b),c)
+            result.append(tmp)
+            if tmp == a:
+                idx[0]+=1
+            if tmp == b:
+                idx[1]+=1
+            if tmp == c:
+                idx[2]+=1
+        return result[k]
